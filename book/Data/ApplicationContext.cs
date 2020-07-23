b3413e5a65cc5859a239df9afcf47e67142be0e1
e.alihangt@gmail.com <e.alihangt@gmail.com>
2020-07-23 13:48:58 +0600

    Добавьте файлы проекта.

---
+++ book/Data/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using book.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace book.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public virtual DbSet<Book> Books { get; set; }
+        public ApplicationContext() : base("name=ApplicationConnectionString")
+        {
+
+        }
+    }
+}
